22811f6dfcee852215bb77e7313293fb99eb9ea8
Faddu Gamers <thefaddugamers@gmail.com>
2025-07-27 19:39:28 +0530

    fix production.opy

--- website/website/production.py
+++ website/website/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-SESSION_COOKIE_SECUR
+SESSION_COOKIE_SECURE = True
 # Ensure that the session cookie is only sent by browsers under an HTTPS connection.
 # https://docs.djangoproject.com/en/stable/ref/settings/#session-cookie-secure
 SESSION_COOKIE_SECURE = True
